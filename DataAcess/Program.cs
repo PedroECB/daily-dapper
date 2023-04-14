ccea78cd61df0804864d53ad505b11e12970b2fd
Pedro Carvalho <pedrosophbc@gmail.com>
2023-04-13 22:07:11 -0300

    First commit

---
+++ DataAcess/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace DataAcess
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost;Port=5432;Database=balta; User ID=postgres;Password=123";
+            //Microsoft.Data.SqlClient
+            Console.WriteLine("Hello, world!");
+            var teste = Guid.NewGuid();
+
+            Console.WriteLine(teste);
+        }
+    }
+}
